1898dd3b7417a9506db610a63f7689d80c3bc679
ivan tkachenko <ratijas@users.noreply.github.com>
2018-12-05 18:43:07 +0300

    cosmit

--- app/Language/Scheme/VM/Core.hs
+++ app/Language/Scheme/VM/Core.hs
@@ -16,83 +16,6 @@ import Text.Parsec.Expr
 import Text.Parsec.Language (emptyDef, javaStyle, LanguageDef)
 import Text.ParserCombinators.Parsec hiding (spaces, string)
 
--- *** Parser
-
-readExpr :: String -> ThrowsError LispVal
-readExpr = readOrThrow parseExpr
-
-
-readExprList :: String -> ThrowsError [LispVal]
-readExprList = readOrThrow (endBy parseExpr spaces)
-
-
-readOrThrow :: Parser a -> String -> ThrowsError a
-readOrThrow parser input = case parse parser "lisp" input of
-    Left err  -> throwError $ Parser err
-    Right val -> return val
-
-
-
-parseExpr :: Parser LispVal
-parseExpr = parseAtom
-         <|> parseString
-         <|> parseNumber
-         <|> parseQuoted
-         <|> do char '('
-                x <- try parseList <|> parseDottedList
-                char ')'
-                return x
-
-
-symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
-
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
-
-
-parseString :: Parser LispVal
-parseString = do
-                char '"'
-                x <- many (noneOf "\"")
-                char '"'
-                return $ String x
-
-
-parseAtom :: Parser LispVal
-parseAtom = do
-              first <- letter <|> symbol
-              rest <- many (letter <|> digit <|> symbol)
-              let atom = first:rest
-              return $ case atom of
-                         "#t" -> Bool True
-                         "#f" -> Bool False
-                         _    -> Atom atom
-
-
-parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
-
-
-parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
-
-
-parseDottedList :: Parser LispVal
-parseDottedList = do
-    head <- endBy parseExpr spaces
-    tail <- char '.' >> spaces >> parseExpr
-    return $ DottedList head tail
-
-
-parseQuoted :: Parser LispVal
-parseQuoted = do
-    char '\''
-    x <- parseExpr
-    return $ List [Atom "quote", x]
-
 
 -- *** D AST
 
@@ -517,6 +440,114 @@ binary match name = Infix parser AssocLeft
                     return (\lhs rhs -> DOp $ DBinaryOp lhs name rhs)
 binaryOp      = binary . reservedOp
 binaryKeyword = binary . reserved
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- *** LISP
+
+-- *** LISP Parser
+
+readExpr :: String -> ThrowsError LispVal
+readExpr = readOrThrow parseExpr
+
+
+readExprList :: String -> ThrowsError [LispVal]
+readExprList = readOrThrow (endBy parseExpr spaces)
+
+
+readOrThrow :: Parser a -> String -> ThrowsError a
+readOrThrow parser input = case parse parser "lisp" input of
+    Left err  -> throwError $ Parser err
+    Right val -> return val
+
+
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
+
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
 
 
 -- *** LispVal
