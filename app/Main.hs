894fb4a19b659dcb221f5e0bcb09a3b2e540f8f0
ivan tkachenko <ratijas@users.noreply.github.com>
2018-12-28 12:24:56 +0300

    cosmit

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,12 @@ import System.Environment
 import Language.Hestu.Core
 
 main :: IO ()
-main = do args <- getArgs
-          case length args of
-               0 -> runRepl
-               1 -> do
-                let filename = args !! 0
-                contents <- readFile filename
-                runOne contents
-               otherwise -> putStrLn "Program takes only 0 or 1 argument"
+main = do
+  args <- getArgs
+  case length args of
+    0 -> runRepl
+    1 -> do
+      let filename = args !! 0
+      contents <- readFile filename
+      runOne contents
+    otherwise -> putStrLn "Program takes only 0 or 1 argument"
